32ae56fc9ae50bf833af266ca1ace77311959322
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2022-10-19 15:55:19 -0500

    Initial commit

---
+++ spec/yondu_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Yondu do
+  it "has a version number" do
+    expect(Yondu::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
