7ee690b4a0f4b5ecf557c0f6d38c57d1f584fb65
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2023-07-20 12:47:02 -0600

    Improve error handling

--- Gemfile
+++ Gemfile
@@ -6,5 +6,5 @@ source "https://rubygems.org"
 gemspec
 
 gem "rake", "~> 13.0"
-gem "rspec", "~> 3.0"
-gem "rubocop", "~> 1.21"
+gem "rspec", "~> 3.12"
+gem "rubocop", "~> 1.50"
--- lib/yondu.rb
+++ lib/yondu.rb
@@ -5,9 +5,15 @@ require_relative "yondu/version"
 module Yondu
   class Error < StandardError; end
 
-  class MissingSetting < Error
-    def initialize(keys)
-      super("Missing setting: #{keys.join('->')}")
+  class MissingSettingError < Error
+    def initialize(scope)
+      super("Missing setting: #{scope.join('->')}")
+    end
+  end
+
+  class NoHashSettingError < Error
+    def initialize(scope)
+      super("No hash setting: #{scope.join('->')}")
     end
   end
 
@@ -21,8 +27,9 @@ module Yondu
       value = @config
 
       keys.each do |key|
+        raise NoHashSettingError.new(scope) unless value.is_a?(Hash)
         scope << key
-        value = value.fetch(key) { raise MissingSetting.new(keys) }
+        value = value.fetch(key) { raise MissingSettingError.new(scope) }
       end
 
       value
